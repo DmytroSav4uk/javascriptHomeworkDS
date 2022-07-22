32db30a3db50ba3f895994a6fdb862f90d42c9ab
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-07-22 17:31:25 +0300

    Initial commit

---
+++ homework1/script.js
@@ -0,0 +1,37 @@
+let s1 = "hello";
+let s2 = "owu";
+let s3 = "com";
+let s4 = "ua";
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let firstName = 'Dmytro';
+let middleName = 'Volodymyr';
+let lastName = 'Savchuk';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+let a = 100;
+let b = '100';
+let c = true;
+alert(s1)
+alert(s2)
+alert(s3)
+alert(s4)
+console.log(s1, s2, s3, s4)
+console.log(person)
+console.log(n1, n2, n3, n4, n5, n6, n7)
+console.log(typeof a, typeof b, typeof c)
+console.log(prompt('enter your name,please'))
+console.log(prompt('enter your last name,please'))
+console.log(prompt('enter your age ,please'))
+document.write(s1+' '+s2+'.'+s3+'.'+s4)
+
+// prompt('write down your name,please')
+// prompt('write down your middle name,please')
+// prompt('write down toy age ,please')
+
+// document.write('hello' + ',' + prompt('write down your name,please')+' '
+//     + prompt('write down your last name,please'))
