52686040f4afda846df6f1150ab58c462e7f260e
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-08-17 00:13:23 +0300

    started 8'th homework

--- homework6/script.js
+++ homework6/script.js
@@ -39,6 +39,7 @@ stringLowerCase('HELLO WORLD');
 stringLowerCase('LOREM IPSUM');
 stringLowerCase('JAVASCRIPT IS COOL');
 
+
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
 let str = ' dirty string   '
